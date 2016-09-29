7fca4d4875dc55109ada6239c9931ec89836f9f7
Nov Matake <nov@matake.jp>
2016-09-29 13:12:07 +0900

    sessions#show

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def show
+    session = merchant.start_session! params[:validation_url]
+    render json: session
+  end
+
+  private
+
+  def merchant
+    client_cert = OpenSSL::X509::Certificate.new(
+      ENV['APPLE_PAY_MERCHANT_IDENTITY_CERT']
+    )
+    private_key = OpenSSL::PKCS12:.new(
+      ENV['APPLE_PAY_MERCHANT_IDENTITY_P12']
+    ).key
+
+    ApplePay::Merchant.new(
+      ENV['APPLE_PAY_MERCHANT_ID'],
+      domain: ENV['APPLE_PAY_MERCHANT_DOMAIN'],
+      display_name: ENV['APPLE_PAY_MERCHANT_DISPLAY_NAME'],
+    ).authenticate(
+      client_cert, private_key
+    )
+  end
+end
