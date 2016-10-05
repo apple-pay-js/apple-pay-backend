f46cba91e6b2cee5f03a8e0595c9ae36b6afb08c
Nov Matake <nov@matake.jp>
2016-10-05 17:20:58 +0900

    make braintree credentials env values
    and revoked legacy one

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = '23fb8326bwd3qqmn'
-Braintree::Configuration.public_key  = '8wcn9g3v4jjp68vv'
-Braintree::Configuration.private_key = '3a5ce1f8bee7d04ed0c070fade0d4d2a'
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key  = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BRAINTREE_PPRIVATE_KEY']
